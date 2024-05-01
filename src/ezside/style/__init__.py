9c59d79bff79573e9da969aaf83fe824ab2dd081
AsgerJon <asgerjon2@gmail.com>
2024-05-01 15:14:16 +0200

    Implementation of entry form ready!

---
+++ src/ezside/style/__init__.py
@@ -0,0 +1,7 @@
+"""The 'style' module provides classes for styling. These are defined by
+core primitives and provides views into higher abstractions."""
+#  GPL-3.0 license
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._base_style import BaseStyle
---
+++ src/ezside/style/_rgb_color.py
@@ -0,0 +1,10 @@
+"""RGB encapsulates red, green and blue composite colour."""
+#  GPL-3.0 license
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ezside.style import BaseStyle
+
+
+class RGB(BaseStyle):
+  """RGB encapsulates red, green and blue composite colour."""
