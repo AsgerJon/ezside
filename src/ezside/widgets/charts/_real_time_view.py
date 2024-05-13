13749ad06951ab7860bf74e677f4d5861bba1893
AsgerJon <asgerjon2@gmail.com>
2024-05-13 03:31:54 +0200

    Exposing chart theme and legend alignment to be defined at instantiation time instead of being hardcoded.

--- src/ezside/widgets/charts/_real_time_view.py
+++ src/ezside/widgets/charts/_real_time_view.py
@@ -6,9 +6,9 @@ from __future__ import annotations
 
 from PySide6.QtCharts import QChartView, QChart
 from PySide6.QtGui import QPainter
-from PySide6.QtWidgets import QWidget
+from vistutils.parse import maybe
 
-from ezside.core import AlignBottom, AlignLeft
+from ezside.core import AlignBottom, AlignLeft, AlignFlag
 from ezside.widgets.charts import DataChart
 
 
@@ -17,11 +17,27 @@ class RealTimeView(QChartView):
   QChart framework."""
 
   __inner_chart__ = None
+  __align_flags__ = None
+  __fallback_align__ = AlignBottom | AlignLeft
+  __chart_theme__ = None
+  __fallback_theme__ = QChart.ChartTheme.ChartThemeBrownSand
 
   def __init__(self, *args, **kwargs) -> None:
     """Initializes the RealTimeView."""
+    fallbackAlign, chartTheme = None, None
+    for arg in args:
+      if isinstance(arg, AlignFlag):
+        if fallbackAlign is None:
+          fallbackAlign = arg
+        else:
+          fallbackAlign |= arg
+      elif isinstance(arg, QChart.ChartTheme):
+        chartTheme = arg
+    self.__align_flags__ = maybe(fallbackAlign, self.__fallback_align__)
+    self.__chart_theme__ = maybe(chartTheme, self.__fallback_theme__)
     self.__inner_chart__ = DataChart(*args, **kwargs)
     self.__inner_chart__.initChart()
+
     QChartView.__init__(self, self.__inner_chart__)
 
   def initUi(self) -> None:
@@ -30,9 +46,9 @@ class RealTimeView(QChartView):
     self.setRenderHint(QPainter.RenderHint.SmoothPixmapTransform)
     self.setRenderHint(QPainter.RenderHint.TextAntialiasing)
     self.__inner_chart__.legend().setVisible(True)
-    self.__inner_chart__.legend().setAlignment(AlignBottom | AlignLeft)
+    self.__inner_chart__.legend().setAlignment(self.__align_flags__)
     self.__inner_chart__.legend().setShowToolTips(True)
-    self.__inner_chart__.setTheme(QChart.ChartTheme.ChartThemeBrownSand)
+    self.__inner_chart__.setTheme(self.__chart_theme__)
 
   def append(self, value: float) -> None:
     """Append a value to the chart."""
