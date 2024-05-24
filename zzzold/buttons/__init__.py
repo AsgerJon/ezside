d00d787e2484309c23ef348b97cb6991fa242369
AsgerJon <asgerjon2@gmail.com>
2024-05-24 21:12:06 +0200

    This commit implements the descriptor protocol to provide for ownerships.

--- zzzold/buttons/__init__.py
+++
@@ -1,9 +0,0 @@
-"""The 'buttons' module provides widgets for buttons and button-like
-widgets. These include push buttons, toggle buttons and button groups. """
-#  GPL-3.0 license
-#  Copyright (c) 2024 Asger Jon Vistisen
-from __future__ import annotations
-
-from ._push_button import PushButton
-from ._check_button import CheckButton
-from ._two_state import TwoState
--- zzzold/buttons/_button_group.py
+++
@@ -1,4 +0,0 @@
-"""ButtonGroup provides a group of buttons. """
-#  GPL-3.0 license
-#  Copyright (c) 2024 Asger Jon Vistisen
-from __future__ import annotations
